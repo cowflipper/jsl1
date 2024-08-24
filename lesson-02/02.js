92e25d9224d3b9c0a27de3c545edd7512b4321bb
Alexandr Zagorovsky <hoff3454@gmail.com>
2024-08-24 19:08:22 +0300

    8

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,22 +7,34 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 = Math.floor(Math.random() * 6); // your code
-let dice2 = Math.floor(Math.random() * 6); // your code
+let dice1; // your code
+let dice2; // your code
 let isWinningDouble = false; // your code
-
-while( dice1 !== dice2 && dice1 < 3) {
-
+while (!isWinningDouble) {
+  dice1 = Math.floor(Math.random() * 6);
+  dice2 = Math.floor(Math.random() * 6);
+  console.log('Первый бросок: ' + dice1);
+console.log('Второй бросок: ' + dice2);
+  if (dice1 === dice2 && dice1 > 3) {
+    isWinningDouble = true;
+    console.log('Выигрышный дубль!')
+  } else {
+    console.log('Не выигрышный дубль.')
+  }
 }
 
 
 
-console.log('Первый бросок: ' + dice1)
-console.log('Второй бросок: ' + dice2)
+/*
+console.log('Первый бросок: ' + dice1);
+console.log('Второй бросок: ' + dice2);
 if (isWinningDouble) {
   console.log('Выигрышный дубль!')
 } else {
   console.log('Не выигрышный дубль.')
 }
-console.log(isWinningDouble);
+*/
+
+
+
 
