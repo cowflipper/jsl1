fce0bec4bf545dc8bc9e7ee4b02307406e8ca93f
Alexandr Zagorovsky <hoff3454@gmail.com>
2024-08-24 18:02:54 +0300

    hw22

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,11 +7,16 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
+let dice1 = Math.floor(Math.random() * 6); // your code
+let dice2 = Math.floor(Math.random() * 6); // your code
+let isWinningDouble = false; // your code
 
-/*
+while( dice1 !== dice2 && dice1 < 3) {
+
+}
+
+
+
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
 if (isWinningDouble) {
@@ -19,4 +24,5 @@ if (isWinningDouble) {
 } else {
   console.log('Не выигрышный дубль.')
 }
-*/
+console.log(isWinningDouble);
+
