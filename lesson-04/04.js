2bc6f3800c5f84b1f4db93d07d20d498a7f3832d
Alexandr Zagorovsky <hoff3454@gmail.com>
2024-09-08 15:53:05 +0300

    hw_4_6

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -12,4 +12,10 @@
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
 
-function doubleEachCharacter() {}
+function doubleEachCharacter(str) {
+    let double;
+    for(let i =0; i < str.length; i++) {
+        double +=str[i] + str[i];
+    }
+    return double;
+}
