c53eb7f8004bfff92c09fc7d2acae27ba3508f91
cowflipper <hoff3454@gmail.com>
2024-09-17 21:19:05 +0300

    hw_5_1

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -35,4 +35,10 @@ const gallery = {
   'The Scream': 'Edvard Munch',
 }
 
-function updateGallery() {}
+function updateGallery(gallery, name, newName) {
+  if(gallery[name] !== undefined){
+    gallery[name] = newName;
+  } else {
+    gallery[name] = newName
+  }
+}
