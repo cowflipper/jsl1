3141ab72957f2ebab58d09d28f7419db530c1498
cowflipper <hoff3454@gmail.com>
2024-09-30 23:26:45 +0300

    les7

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -6,4 +6,11 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
-function capitalizeWords() {}
+function capitalizeWords (str) {
+  const word = str.split(' ');
+  for(let i = 0; i < word.length; i++) {
+      word[i] = word[i][0].toUpperCase() + word[i].slice(1).toLowerCase();
+  } return word.join(' ');
+} 
+console.log(capitalizeWords('hello world from'));
+
